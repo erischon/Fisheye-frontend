3b433625173f198340549c535646c971e623e784
Eri Schon <erischon@gmail.com>
2022-06-27 14:56:59 +0200

    Update photographer.js

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,10 +1,12 @@
 function photographerFactory(data) {
-  const { name, city, country, tagline, price, portrait } = data;
+  const { id, name, city, country, tagline, price, portrait } = data;
 
   const picture = `assets/photographers/${portrait}`;
 
   function getUserCardDOM() {
     const article = document.createElement("article");
+    const linkBox = document.createElement("div");
+    const link = document.createElement("a");
     const img = document.createElement("img");
     const h2 = document.createElement("h2");
     const desc = document.createElement("div");
@@ -12,22 +14,28 @@ function photographerFactory(data) {
     const line2 = document.createElement("span");
     const line3 = document.createElement("span");
 
+    const url = `photographer/${id}`;
+
     img.setAttribute("src", picture);
     h2.textContent = name;
+    link.setAttribute("href", url);
     line1.textContent = `${city}, ${country}`;
     line2.textContent = tagline;
     line3.textContent = `${price}€/jour`;
 
+    linkBox.classList.add("card__linkBox");
     line1.classList.add("card__location");
     line2.classList.add("card__tagline");
     line3.classList.add("card__price");
 
-    article.appendChild(img);
-    article.appendChild(h2);
+    article.appendChild(link);
     article.appendChild(desc);
-    article.appendChild(line1);
-    article.appendChild(line2);
-    article.appendChild(line3);
+    link.appendChild(linkBox);
+    linkBox.appendChild(img);
+    linkBox.appendChild(h2);
+    desc.appendChild(line1);
+    desc.appendChild(line2);
+    desc.appendChild(line3);
     return article;
   }
   return { name, picture, getUserCardDOM };
