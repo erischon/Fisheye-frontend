e5a1b156f0a3f89ddfeaf2cf2000f598bce28e7d
Eri Schon <erischon@gmail.com>
2022-07-19 17:05:44 +0200

    Update lightbox.controller.js

--- scripts/controllers/lightbox.controller.js
+++ scripts/controllers/lightbox.controller.js
@@ -6,31 +6,33 @@
  */
 
 export class Lightbox {
-  static init() {
-    const links = Array.from(
-      document.querySelectorAll(
-        ".photographer-media__image"
-        // ".photographer-media__image, .photographer-media__video"
-      )
-    );
+  static init(media) {
+    const mediaList = Array.from(media);
 
-    const images = links.map((link) => {
-      if (link.classList.value === "photographer-media__video") {
-        return link.firstElementChild.getAttribute("src");
-      } else if (link.classList.value === "photographer-media__image") {
-        return link.getAttribute("src");
+    const mediaLinks = mediaList.map((media) => {
+      if (media.constructor.name === "MediaVideo") {
+        return media.video;
+      } else if (media.constructor.name === "MediaImage") {
+        return media.image;
       }
     });
 
-    // const images = links.map((link) => link.getAttribute("src"));
-    const imagesTitles = links.map((link) => link.getAttribute("alt"));
+    const mediaTitles = mediaList.map((media) => media.title);
 
-    links.forEach((link) =>
-      link.addEventListener("click", (e) => {
+    mediaList.forEach((media) => {
+      const html = document.getElementsByClassName(`${media.id}`);
+
+      html[0].addEventListener("click", (e) => {
         e.preventDefault();
-        new Lightbox(e.currentTarget.getAttribute("src"), images, imagesTitles);
-      })
-    );
+
+        new Lightbox(
+          media.constructor.name,
+          media.image,
+          mediaLinks,
+          mediaTitles
+        );
+      });
+    });
   }
 
   /**
@@ -38,12 +40,17 @@ export class Lightbox {
    * @param {string[]} images Images paths
    * @param {string[]} imagesTitles Media titles
    */
-  constructor(url, images, imagesTitles) {
+  constructor(type, url, images, imagesTitles) {
     this.element = this.buildDom(url);
     this.images = images;
     this.imagesTitles = imagesTitles;
     this.onKeyUp = this.onKeyUp.bind(this);
-    this.loadImage(url);
+    if (type === "MediaImage") {
+      this.loadImage(url);
+    }
+    if (type === "MediaVideo") {
+      this.loadVideo(url);
+    }
 
     document.body.appendChild(this.element);
     document.addEventListener("keyup", this.onKeyUp);
@@ -92,6 +99,38 @@ export class Lightbox {
     };
 
     image.src = url;
+  }
+
+  /**
+   * Load the image
+   * @param {string} url Image URL
+   */
+  loadVideo(url) {
+    console.log("video");
+    // this.url = null;
+    // const image = new Image();
+    // const container = this.element.querySelector(".lightbox__container");
+
+    // // Loader
+    // const loader = document.createElement("div");
+    // loader.classList.add("lightbox__loader");
+    // container.innerHTML = "";
+    // container.appendChild(loader);
+
+    // // Image Title
+    // const i = this.images.findIndex((image) => image === url);
+    // const imageTitle = document.createElement("p");
+    // imageTitle.classList.add("lightbox__title");
+    // imageTitle.textContent = this.imagesTitles[i];
+
+    // image.onload = () => {
+    //   container.removeChild(loader);
+    //   container.appendChild(image);
+    //   container.appendChild(imageTitle);
+    //   this.url = url;
+    // };
+
+    // image.src = url;
   }
 
   /**
